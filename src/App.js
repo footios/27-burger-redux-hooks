97a6199372586b95bfd99a46d3043e4e690dab62
Fotis Tsakiris <footios76@gmail.com>
2019-04-11 10:26:45 +0300

    app converted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { useEffect }  from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+
 import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 //import Checkout from './containers/Checkout/Checkout';
 // import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Logout/Logout';
+// import Auth from './containers/Auth/Auth';
+// import Logout from './containers/Logout/Logout';
 import { connect } from 'react-redux';
 import * as actions from './store/actions';
 import Spinner from './components/UI/Spinner/Spinner';
@@ -20,47 +21,47 @@ const asyncOrders = asyncComponent(() => {
 	return import('./containers/Orders/Orders');
 });
 
-// const asyncAuth = asyncComponent(() => {
-// 	return import('./containers/Auth/Auth');
-// });
+const asyncAuth = asyncComponent(() => {
+	return import('./containers/Auth/Auth');
+});
 
 const asyncLogout = asyncComponent(() => {
 	return import('./containers/Logout/Logout');
 });
 
-class App extends Component {
-	componentDidMount() {
-		this.props.onTryAutoSignup();
-	}
 
-	render() {
-		let routes = (
+const app = (props) => {
+	
+	useEffect(() => {
+		props.onTryAutoSignup();
+	}, []);
+
+	let routes = (
+		<Switch>
+			<Route path="/auth" exact component={asyncAuth} />
+			<Route path="/" exact component={BurgerBuilder} />
+			{/* <Redirect to='/'/>  this is now reduntant because of  isAuthInitialized */}
+		</Switch>
+	);
+	if (props.isAuthenticated) {
+		routes = (
 			<Switch>
-				<Route path="/auth" exact component={Auth} />
+				{/* With just 'exact' the order doesn't matter, but with Switch it does! */}
+				{/* The 'exact' in the Route with path='/checkout' was preventing the ContactData to render */}
+				<Route path="/checkout" component={asyncCheckout} />
+				<Route path="/orders" exact component={asyncOrders} />
+				<Route path="/auth" exact component={asyncAuth} />
+				<Route path="/logout" exact component={asyncLogout} />
 				<Route path="/" exact component={BurgerBuilder} />
-				{/* <Redirect to='/'/>  this is now reduntant because of  isAuthInitialized */}
 			</Switch>
 		);
-		if (this.props.isAuthenticated) {
-			routes = (
-				<Switch>
-					{/* With just 'exact' the order doesn't matter, but with Switch it does! */}
-					{/* The 'exact' in the Route with path='/checkout' was preventing the ContactData to render */}
-					<Route path="/checkout" component={asyncCheckout} />
-					<Route path="/orders" exact component={asyncOrders} />
-					<Route path="/auth" exact component={Auth} />
-					<Route path="/logout" exact component={asyncLogout} />
-					<Route path="/" exact component={BurgerBuilder} />
-				</Switch>
-			);
-		}
-		return (
-			<div>
-				<Layout>{this.props.isAuthInitialized ? routes : <Spinner />}</Layout>
-			</div>
-		);
 	}
-}
+	return (
+		<div>
+			<Layout>{props.isAuthInitialized ? routes : <Spinner />}</Layout>
+		</div>
+	);
+};
 
 const mapStateToProps = (state) => {
 	return {
@@ -75,4 +76,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
